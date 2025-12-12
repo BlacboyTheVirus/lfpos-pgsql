45e63bdaf6b733aba009915c97e4617b00fe13bc
blacboy <aodeyemi@gmail.com>
2025-12-12 16:57:21 +0100

    FEAT: Returned RBAC

--- app/Filament/Widgets/TopCustomersWidget.php
+++ app/Filament/Widgets/TopCustomersWidget.php
@@ -21,6 +21,8 @@ class TopCustomersWidget extends BaseWidget
 
     protected static ?int $sort = 4;
 
+    protected static bool $isLazy = false;
+
     protected int|string|array $columnSpan = [
         'default' => 'full',
         'sm' => 'full',
--- resources/views/filament/widgets/date-range-filter.blade.php
+++ resources/views/filament/widgets/date-range-filter.blade.php
@@ -8,7 +8,7 @@
             @foreach (['all' => 'All Time', 'today' => 'Today', 'last_7_days' => 'Last 7 Days', 'this_week' => 'This Week', 'last_30_days' => 'Last 30 Days', 'this_month' => 'This Month', 'this_year' => 'This Year'] as $value => $label)
                 <button
                     @click="$dispatch('setDateRange', { range: '{{ $value }}' }); document.querySelector('[data-livewire-component]')._x_livewire.setDateRange('{{ $value }}')"
-                    class="px-4 py-2 text-sm font-medium rounded-lg transition bg-gray-100 text-gray-700 hover:bg-gray-200 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
+                    class="px-4 py-2 text-sm font-medium rounded-lg transition bg-gray-700 text-white hover:bg-gray-800 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
                 >
                     {{ $label }}
                 </button>
